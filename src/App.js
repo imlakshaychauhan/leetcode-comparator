0aa93c471d92a7feb2f5416f4140a260ce83391b
imlakshaychauhan <imlakshaychauhan@gmail.com>
2022-09-07 12:54:19 +0530

    Trying deployment

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
 import Header from "./components/Header";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
 import Compare from './pages/Compare'
 import './app.css';
 
 const App = () => {
   return (
-      <HashRouter>
-        <Header />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/compare" element={<Compare />} />
-          <Route path="*" element={<Home />} />
-        </Routes>
-    </HashRouter>
+      <BrowserRouter>
+            <Header />
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/compare" element={<Compare />} />
+              <Route path="*" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
   );
 };
 
