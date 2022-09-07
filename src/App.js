1a3ca609181cc1e009897c10c9d411c64a642bae
imlakshaychauhan <imlakshaychauhan@gmail.com>
2022-09-07 12:10:42 +0530

    Ready for deployment-1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
 import Header from "./components/Header";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
 import Compare from './pages/Compare'
 import './app.css';
 
 const App = () => {
   return (
-      <Router>
+      <HashRouter>
         <Header />
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/compare" element={<Compare />} />
           <Route path="*" element={<Home />} />
         </Routes>
-    </Router>
+    </HashRouter>
   );
 };
 
